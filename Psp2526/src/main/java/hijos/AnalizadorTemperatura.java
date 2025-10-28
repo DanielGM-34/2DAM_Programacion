0fa7ac07e6a5d9bbe9464aae36bdb9496b525ef6
DanielGM-34 <dgarmen630@g.educaand.es>
2025-10-28 21:22:34 +0100

    psp temperatura

--- Psp2526/src/main/java/hijos/AnalizadorTemperatura.java
+++ Psp2526/src/main/java/hijos/AnalizadorTemperatura.java
@@ -13,44 +13,62 @@ public class AnalizadorTemperatura {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String nombreFicheroTemp = args[0]; 
+		String nombreFicheroTemp = args[0];
 		int umbral = Integer.parseInt(args[1]);
 		String nombreFicheroResult = args[2];
 		AnalizadorTemperatura p = new AnalizadorTemperatura();
-		try { 
+		try {
 			p.analizaTemperatura(nombreFicheroTemp, umbral);
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} 
+		}
 
-	} 
+	}
 
-	public int analizaTemperatura(String ficheroYRuta, int umbral)
-			throws FileNotFoundException {
-		int tempMayorUmbral = 0; 
+	public int analizaTemperaturaSinEscribir(String ficheroYRuta, int umbral) throws FileNotFoundException {
+		int tempMayorUmbral = 0;
+		FileReader fichero = new FileReader(ficheroYRuta);
+		Scanner in = new Scanner(fichero);
+
+		while (in.hasNextLine()) {
+			String tempString = in.nextLine();
+			int temperatura = Integer.parseInt(tempString);
+			if (temperatura > umbral) {
+				tempMayorUmbral += 1;
+			}
+		}
+
+		in.close();
+		System.out.println("Umbral " + umbral + " → " + tempMayorUmbral + " días");
+
+		return tempMayorUmbral;
+	}
+
+	public int analizaTemperatura(String ficheroYRuta, int umbral) throws FileNotFoundException {
+		int tempMayorUmbral = 0;
 		File ficheroS = new File(ficheroYRuta);
 		FileReader fichero = new FileReader(ficheroYRuta);
 		Scanner in = new Scanner(fichero);
 		while (in.hasNextLine()) {
 			String tempString = in.nextLine();
 			int temperatura = Integer.parseInt(tempString);
-			if (temperatura > umbral) {  
-				tempMayorUmbral += 1; 
-				this.escribreTemperatura(tempMayorUmbral, rutaResoruces +umbral+".txt");
-			} 
+			if (temperatura > umbral) {
+				tempMayorUmbral += 1;
+				this.escribreTemperatura(tempMayorUmbral, rutaResoruces + umbral + ".txt");
+			}
 
-		} 
+		}
 		in.close();
 		return tempMayorUmbral;
-	} 
- 
+	}
+
 	public void escribreTemperatura(int temperatura, String rutaFichero) {
 		FileWriter ficheroSalida = null;
 		try {
 			ficheroSalida = new FileWriter(rutaFichero);
 			String tempString = String.valueOf(temperatura);
-			ficheroSalida.write(tempString);  
+			ficheroSalida.write(tempString);
 
 		} catch (IOException e) {
 			System.out.println("IOException");
@@ -63,48 +81,40 @@ public class AnalizadorTemperatura {
 					e.printStackTrace();
 				}
 		}
-	} 
-	
-	public int analizaTemperaturaConOutPrintF(String ficheroYRuta, int umbral)
-	        throws FileNotFoundException {
-	    int tempMayorUmbral = 0;
-	    FileReader fichero = new FileReader(ficheroYRuta);
-	    Scanner in = new Scanner(fichero);
-
-	    while (in.hasNextLine()) {
-	        int temperatura = Integer.parseInt(in.nextLine());
-	        if (temperatura > umbral) {
-	            tempMayorUmbral++;
-	        }
-	    }
-	    in.close();
-
-	    System.out.printf("Umbral: %d Temperaturas mayores: %d%n", umbral, tempMayorUmbral);
-	    escribreTemperaturaConOutPrintf(tempMayorUmbral, rutaResoruces + umbral + ".txt");
-
-	    return tempMayorUmbral;
 	}
-	
+
+	public int analizaTemperaturaConOutPrintF(String ficheroYRuta, int umbral) throws FileNotFoundException {
+		int tempMayorUmbral = 0;
+		FileReader fichero = new FileReader(ficheroYRuta);
+		Scanner in = new Scanner(fichero);
+
+		while (in.hasNextLine()) {
+			int temperatura = Integer.parseInt(in.nextLine());
+			if (temperatura > umbral) {
+				tempMayorUmbral++;
+			}
+		}
+		in.close();
+
+		System.out.printf("Umbral: %d Temperaturas mayores: %d%n", umbral, tempMayorUmbral);
+		escribreTemperaturaConOutPrintf(tempMayorUmbral, rutaResoruces + umbral + ".txt");
+
+		return tempMayorUmbral;
+	}
+
 	public void escribreTemperaturaConOutPrintf(int temperatura, String rutaFichero) {
-	    PrintWriter out = null;
-	    try {
-	        out = new PrintWriter(rutaFichero);
-	        out.printf("%d%n", temperatura); // Escribe el número con salto de línea
-	        System.out.printf("Resultado guardado en '%s': %d%n", rutaFichero, temperatura);
-	    } catch (IOException e) {
-	        System.out.printf("IOException al escribir en '%s'%n", rutaFichero);
-	    } finally {
-	        if (out != null) {
-	            out.close();
-	        }
-	    }
+		PrintWriter out = null;
+		try {
+			out = new PrintWriter(rutaFichero);
+			out.printf("%d%n", temperatura); // Escribe el número con salto de línea
+			System.out.printf("Resultado guardado en '%s': %d%n", rutaFichero, temperatura);
+		} catch (IOException e) {
+			System.out.printf("IOException al escribir en '%s'%n", rutaFichero);
+		} finally {
+			if (out != null) {
+				out.close();
+			}
+		}
 	}
-
-
-	
-
-	
-	
-	
 
 }
--- Psp2526/src/main/java/padres/LnazadorAnalisisTemperaturas.java
+++ Psp2526/src/main/java/padres/LnazadorAnalisisTemperaturas.java
@@ -1,11 +1,14 @@
 package padres;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List; 
+import java.util.List;
+import java.util.Scanner;
 
 public class LnazadorAnalisisTemperaturas {
 	private static final String rutaResources = "src/main/resources/";
@@ -13,42 +16,90 @@ public class LnazadorAnalisisTemperaturas {
 	private final static String rutaSource = "src/main/java/";
 
 	public static void main(String[] args) {
-		
-		LnazadorAnalisisTemperaturas pepe = new LnazadorAnalisisTemperaturas();
-		pepe.compilaClase("hijos/AnalizadorTemperatura.java");
-		int[] umbrales = { 40,10, 20, 25, 30, 35,41}; 
-		int[] salidas = new int[umbrales.length];
-		
-		Process[] procesos = new Process[umbrales.length];
-		for (int i = 0; i < umbrales.length; i++) {
-			procesos[i] = pepe.ejecutaProceso(umbrales[i]);
-		}
-		for (int i = 0; i < umbrales.length; i++) 
-		{
+
+		int[] umbrales = { 20, 5, 1, 40, 30, 35, 41,8,0 };
+		LnazadorAnalisisTemperaturas pepe = new LnazadorAnalisisTemperaturas(); // tu clase que ejecuta procesos
+		Process[] procesosPepe = pepe.lanzarProcesos(umbrales, pepe);
+		int[] salidas = pepe.esperarProcesos(procesosPepe);
+
+		// hacer funcionLeeFichero
+		pepe.muestraTodosLosFicheros(umbrales);
+
+		/*
+		 * LnazadorAnalisisTemperaturas pepe = new LnazadorAnalisisTemperaturas();
+		 * pepe.compilaClase("hijos/AnalizadorTemperatura.java"); int[] umbrales = {
+		 * 20,5, 1, 40, 30, 35,41}; int[] salidas = new int[umbrales.length];
+		 * 
+		 * Process[] procesosPepe = lanzarProcesos(umbrales, pepe); int[] salidas =
+		 * esperarProcesos(procesosPepe);
+		 * 
+		 */
+
+		/*
+		 * Process[] procesos = new Process[umbrales.length]; for (int i = 0; i <
+		 * umbrales.length; i++) { procesos[i] = pepe.ejecutaProceso(umbrales[i]); } for
+		 * (int i = 0; i < umbrales.length; i++) { try {
+		 * salidas[i]=procesos[i].waitFor(); } catch (InterruptedException e) { // TODO
+		 * Auto-generated catch block e.printStackTrace(); }
+		 */
+
+	}
+
+	// TODO Auto-generated method stub
+
+	public void muestraTodosLosFicheros(int[] umbrales) {
+		System.out.println("Resultados:");
+		for (int tempeUmbral : umbrales) {
+			String rutaFIchero = rutaResources + tempeUmbral + ".txt";
+
+			System.out.println("Umbral " + tempeUmbral + ":");
 			try {
-				salidas[i]=procesos[i].waitFor();
-			} catch (InterruptedException e) {
+				muestraContenidoFich(rutaFIchero);
+			} catch (FileNotFoundException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
-		// hacer funcionLeeFichero
-		/*
-		try {
-			
-			
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		*/
+
 	}
 
-		// TODO Auto-generated method stub
+	public void muestraContenidoFich(String rutaYNombre) throws FileNotFoundException {
+		Scanner in = null;
+		try {
+			FileReader fichero = new FileReader(rutaYNombre);
+			in = new Scanner(fichero);
+			while (in.hasNext()) {
+				System.out.println(in.next() + " → dias"); // lectura palabra a palabra
+			}
+		} finally {
+			if (in != null) {
+				in.close();
+			}
+		}
+	}
 
+	public Process[] lanzarProcesos(int[] umbrales, LnazadorAnalisisTemperaturas pepe) {
+		Process[] procesos = new Process[umbrales.length];
+		for (int i = 0; i < umbrales.length; i++) {
+			procesos[i] = pepe.ejecutaProceso(umbrales[i]);
+		}
+		return procesos;
+	}
 
+	public int[] esperarProcesos(Process[] procesos) {
+		int[] salidas = new int[procesos.length];
+		for (int i = 0; i < procesos.length; i++) {
+			try {
+				salidas[i] = procesos[i].waitFor();
+			} catch (InterruptedException e) {
+				System.err.println("Error esperando proceso " + i);
+				e.printStackTrace(); 
+			}
+		}
+		return salidas;
+	}
 
-	public void compilaClase(String ruta) { 
+	public void compilaClase(String ruta) {
 		// Compilar el archivo fuente y generar el .class en el directorio target
 		String[] comando = { "javac", "-d", directorioGenerarClasses, rutaSource + ruta };
 		ProcessBuilder pb = new ProcessBuilder(comando);
@@ -63,47 +114,39 @@ public class LnazadorAnalisisTemperaturas {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public Process ejecutaProcesoGPT(int umbral) {
-	    Process proceso = null;
-	    String[] comando = {
-	        "java",
-	        "-cp",
-	        "target/classes",
-	        "hijos.AnalizadorTemperatura",
-	        "src/main/resources/datos.txt",
-	        String.valueOf(umbral),
-	        "src/main/resources/" + umbral + ".txt"
-	    };
+		Process proceso = null;
+		String[] comando = { "java", "-cp", "target/classes", "hijos.AnalizadorTemperatura",
+				"src/main/resources/datos.txt", String.valueOf(umbral), "src/main/resources/" + umbral + ".txt" };
 
-	    ProcessBuilder pb = new ProcessBuilder(comando);
+		ProcessBuilder pb = new ProcessBuilder(comando);
 
-	    try {
-	        proceso = pb.start();
-	        int exit = proceso.waitFor();
-	        System.out.println("Proceso terminado con código: " + exit);
+		try {
+			proceso = pb.start();
+			int exit = proceso.waitFor();
+			System.out.println("Proceso terminado con código: " + exit);
 
-	        if (exit != 0) {
-	            BufferedReader errorReader = new BufferedReader(new InputStreamReader(proceso.getErrorStream()));
-	            String errorLinea;
-	            while ((errorLinea = errorReader.readLine()) != null) {
-	                System.err.println("Error Padre: " + errorLinea);
-	            }
-	        } else {
-	            BufferedReader reader = new BufferedReader(new InputStreamReader(proceso.getInputStream()));
-	            String linea;
-	            while ((linea = reader.readLine()) != null) {
-	                System.out.println("Padre: " + linea);
-	            }
-	        }
+			if (exit != 0) {
+				BufferedReader errorReader = new BufferedReader(new InputStreamReader(proceso.getErrorStream()));
+				String errorLinea;
+				while ((errorLinea = errorReader.readLine()) != null) {
+					System.err.println("Error Padre: " + errorLinea);
+				}
+			} else {
+				BufferedReader reader = new BufferedReader(new InputStreamReader(proceso.getInputStream()));
+				String linea;
+				while ((linea = reader.readLine()) != null) {
+					System.out.println("Padre: " + linea);
+				}
+			}
 
-	    } catch (IOException | InterruptedException e) {
-	        e.printStackTrace();
-	    }
+		} catch (IOException | InterruptedException e) {
+			e.printStackTrace();
+		}
 
-	    return proceso;
+		return proceso;
 	}
-
 
 	/*
 	 * public Process ejecutaProcesoCompila() { // Ejecutar la clase compilada
@@ -130,8 +173,9 @@ public class LnazadorAnalisisTemperaturas {
 		// Ejecutar la clase compilada usando su nombre de clase, no la ruta al .java
 		// doy dos argumentos hola y adios
 		String[] comando = { "java", "-cp", directorioGenerarClasses, "hijos.AnalizadorTemperatura",
-				"src/main/resources/datos.txt", String.valueOf(umbral), "hijos.AnalizadorTemperatura",
+				"src/main/resources/datos.txt", String.valueOf(umbral),
 				"src/main/resources/" + String.valueOf(umbral) + ".txt" };
+
 		ProcessBuilder pb = new ProcessBuilder(comando);
 		try {
 			// pb.redirectErrorStream(true);
@@ -157,7 +201,7 @@ public class LnazadorAnalisisTemperaturas {
 				}
 
 			}
- 
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		} catch (InterruptedException e) {
@@ -165,46 +209,43 @@ public class LnazadorAnalisisTemperaturas {
 		}
 		return proceso;
 	}
-	
-	
-	
-	
+
 	public Process ejecutarClasePlantilla(String className, String classpath, String... args) {
-	    Process proceso = null;
+		Process proceso = null;
 
-	    List<String> comando = new ArrayList<String>();
-	    comando.add("java");
-	    comando.add("-cp");
-	    comando.add(classpath); 
-	    comando.add(className);
-	    comando.addAll(Arrays.asList(args));
+		List<String> comando = new ArrayList<String>();
+		comando.add("java");
+		comando.add("-cp");
+		comando.add(classpath);
+		comando.add(className);
+		comando.addAll(Arrays.asList(args));
 
-	    ProcessBuilder pb = new ProcessBuilder(comando);
+		ProcessBuilder pb = new ProcessBuilder(comando);
 
-	    try {
-	        proceso = pb.start();
-	        int exitCode = proceso.waitFor();
-	        System.out.println("Código de salida: " + exitCode);
+		try {
+			proceso = pb.start();
+			int exitCode = proceso.waitFor();
+			System.out.println("Código de salida: " + exitCode);
 
-	        if (exitCode != 0) {
-	            BufferedReader errorReader = new BufferedReader(new InputStreamReader(proceso.getErrorStream()));
-	            String lineaError;
-	            while ((lineaError = errorReader.readLine()) != null) {
-	                System.err.println("Error: " + lineaError);
-	            }
-	        } else {
-	            BufferedReader outputReader = new BufferedReader(new InputStreamReader(proceso.getInputStream()));
-	            String lineaSalida;
-	            while ((lineaSalida = outputReader.readLine()) != null) {
-	                System.out.println("Salida: " + lineaSalida);
-	            }
-	        }
+			if (exitCode != 0) {
+				BufferedReader errorReader = new BufferedReader(new InputStreamReader(proceso.getErrorStream()));
+				String lineaError;
+				while ((lineaError = errorReader.readLine()) != null) {
+					System.err.println("Error: " + lineaError);
+				}
+			} else {
+				BufferedReader outputReader = new BufferedReader(new InputStreamReader(proceso.getInputStream()));
+				String lineaSalida;
+				while ((lineaSalida = outputReader.readLine()) != null) {
+					System.out.println("Salida: " + lineaSalida);
+				}
+			}
 
-	    } catch (IOException | InterruptedException e) {
-	        e.printStackTrace();
-	    }
+		} catch (IOException | InterruptedException e) {
+			e.printStackTrace();
+		}
 
-	    return proceso;
+		return proceso;
 	}
 
 }
